0ac55e9f7dee589efa21ae36e8dbceadb888df2a
Michel Lenhart <80361711+michellenhart@users.noreply.github.com>
2022-12-06 19:18:02 -0300

    aaa

--- tree.c
+++ tree.c
@@ -2,17 +2,112 @@
 #include <string.h>
 #include <locale.h>
 #include <stdlib.h>
-#include "data.h"
 #include "sqlite-amalgamation-3400000\sqlite3.h"
 
 #define GRAU 6
 
 typedef struct tipo_no {
     int *treino;
+    int nro_repostas;
     struct tipo_no *aponta[GRAU+1];
 } tipo_no;
 
 tipo_no *raiz;
+sqlite3 *db;
+sqlite3_stmt *stmt;
+sqlite3_stmt *stmtaux;
+
+void print_col_value(sqlite3_stmt* stmt, int col) {
+
+  	int colType = sqlite3_column_type(stmt, col);
+
+	switch(colType){
+
+		case SQLITE_INTEGER:
+	         printf("%3d", sqlite3_column_int(stmt, col));
+	         break;
+	
+	    case SQLITE_FLOAT:
+	         printf("%5.2f", sqlite3_column_double(stmt, col));
+	         break;
+	
+	    case SQLITE_TEXT:
+	         printf("%-5s", sqlite3_column_text(stmt, col));
+	         break;
+	
+	    case SQLITE_NULL:
+	         printf("null");
+	         break;
+	
+	    case SQLITE_BLOB:
+	         printf("blob");
+	         break;
+	}
+}
+
+int abre_db(){
+	int saida = 0;
+	
+	saida = sqlite3_open("workout_guru.db", &db);
+	
+	if (saida){
+		printf("NAO FOI POSSIVEL CONECTAR AO BANCO: %s\n", sqlite3_errmsg(db));
+		return 0;
+	}
+	printf("CONECTADO AO BANCO.\n");	
+}
+
+void fecha_db(){	    
+    sqlite3_finalize(stmt);
+    sqlite3_close(db);	
+}
+
+void select_perguntas(){
+    if (sqlite3_prepare_v2(db, "SELECT Q FROM PERGUNTAS", -1, &stmt, NULL)){
+       printf("Erro ao executar o select.\n");
+       sqlite3_close(db);
+       exit(-1);
+    }	
+}
+
+int printa_perguntas(){
+	if (sqlite3_step(stmt) != SQLITE_DONE){
+     	printf("\n");
+        print_col_value(stmt, 0);
+        return 1;
+    }
+    else
+    	return 0;
+}
+
+int get_recordcount(){
+	if (sqlite3_prepare_v2(db, "SELECT COUNT(Q) FROM PERGUNTAS", -1, &stmt, NULL)){
+       printf("Erro ao executar o select.\n");
+       sqlite3_close(db);
+       return -1;
+    }
+	else{
+		sqlite3_step(stmt);
+		return sqlite3_column_int(stmt, 0);	
+	}	
+}
+
+int sql_possibleanswers(){
+	if (sqlite3_prepare_v2(db, "SELECT QTDANSWERS FROM PERGUNTAS", -1, &stmtaux, NULL)){
+       printf("Erro ao executar o select.\n");
+       sqlite3_close(db);
+       return 0;
+    }
+    else
+    	return 1;
+}
+
+int get_possibleanswers(){
+	if (sqlite3_step(stmtaux) != SQLITE_DONE)
+		return sqlite3_column_int(stmtaux, 0);	
+	else
+		return -1;
+}
 
 void inicializaArvore(){
     raiz = malloc( sizeof( tipo_no ) );
@@ -85,15 +180,24 @@ tipo_no* novoNodo() {
     return novo;
 }
 
-void cria_arvore(tipo_no *no){
-	tipo_no *aux = no;
+void cria_arvore(tipo_no *no, sqlite3_stmt *stmts){
+	int i;
+	sqlite3_stmt *stmt1 = stmts;
 	
-	for (i = 0; i < recordcount; i++){
-		for(j = 0; j < get_possibleanswers(); j++){
-			aux->aponta[j] = novoNodo();
-			aux->aponta[j]->treino = NULL;
+	if(sqlite3_step(stmtaux) == SQLITE_ROW){
+		no->nro_repostas = sqlite3_column_int(stmtaux,0);
+		for(i = 0; i < no->nro_repostas; i++){
+			no->aponta[i] = novoNodo();
+			no->aponta[i]->treino = 0;
+			cria_arvore(no->aponta[i],stmtaux);
 		}
+		//sqlite3_step(stmts);
+		/*for (i = 0; i < no->nro_repostas; i++){
+			cria_arvore(no->aponta[i],stmts);
+		}*/
 	}
+	else{
+	}		
 }
 
 void criar_treino(tipo_no *no){
@@ -102,7 +206,7 @@ void criar_treino(tipo_no *no){
 	abre_db();
 	recordcount = get_recordcount();
 	sql_possibleanswers();
-	cria_arvore(no);
+	cria_arvore(no,stmtaux);
 	select_perguntas();
 	while(printa_perguntas()){
 		answer = scanf("%i");	
